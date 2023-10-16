7ad28ede1490972f9f42bf79fb0ae4c9e1a2c684
Danny Aldham <daniel.aldham@gmail.com>
2023-10-16 23:48:34 +0200

    Complete task 07 temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let ans = (num - 32) * (5/9)
+  return parseFloat(ans.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let ans = (num * (9/5) + 32)
+  return parseFloat(ans.toFixed(1))
 };
 
 // Do not edit below this line
