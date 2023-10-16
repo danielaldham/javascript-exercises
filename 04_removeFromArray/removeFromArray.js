09568d91f31d304a655ec55b54e3da5c7300ed2e
Danny Aldham <daniel.aldham@gmail.com>
2023-10-16 23:01:37 +0200

    Complete task 04 remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    let lose = [];
+    let final = [];
+    for (let i = 1; i < arguments.length; i++) {
+        lose.push(arguments[i]);
+    }
+    console.log(array)
+    console.log(lose)
+    for (const i of array) {
+        if (lose.indexOf(i) === -1) {
+            final.push(i)
+        }
+    }
+    return final;
 };
 
 // Do not edit below this line
