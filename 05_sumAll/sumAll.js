66c86064e1b0d3b9aebdbb2fda3b07f803e09361
Danny Aldham <daniel.aldham@gmail.com>
2023-10-16 23:22:44 +0200

    complete task 05 sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+let sum = 0;
+if (finish < start) {
+    [start, finish] = [finish, start];
+}
+if (start < 0 || finish < 0) {
+    return "ERROR"
+}
+if (typeof start != "number" || typeof finish != "number") {
+    return "ERROR"
+}
+for (let i = start; i <= finish; i++) {
+    sum += i;
+}
+return sum
 };
 
 // Do not edit below this line
