ee99bfc829e23bad578046a513ede10eac6b0e30
Danny Aldham <daniel.aldham@gmail.com>
2023-10-16 23:02:33 +0200

    clean up task 04 code remove console logs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -5,8 +5,6 @@ const removeFromArray = function() {
     for (let i = 1; i < arguments.length; i++) {
         lose.push(arguments[i]);
     }
-    console.log(array)
-    console.log(lose)
     for (const i of array) {
         if (lose.indexOf(i) === -1) {
             final.push(i)
