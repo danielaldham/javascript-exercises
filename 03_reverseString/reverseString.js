b847c1ccc1759f9943a0d3d9f54010c698d102e4
Danny Aldham <daniel.aldham@gmail.com>
2023-10-16 22:44:24 +0200

    Completed task 03 reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const inputLength = input.length - 1;
+    let newString = '';
+    for (let i = inputLength; i >= 0; i--) {
+        newString += input[i];
+    }
+    return newString
 };
 
 // Do not edit below this line
